d3d0f71a32bb147211aa261122c72d68ad46f82a
LeiWithP <pedro.barrita1029@gmail.com>
2022-10-13 00:36:35 -0500

    Prueba de conexion local con postgres exitosa!

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,3 +8,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    tfno = models.CharField(max_length=7)
