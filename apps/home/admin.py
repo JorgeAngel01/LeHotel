993baf948bf5956d95f2fc60cedc350efc397d51
LeiWithP <pedro.barrita1029@gmail.com>
2022-10-15 22:28:09 -0500

    Tablas V3 de la base de datos agregadas

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -4,16 +4,39 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from .models import Habitaciones, MediosPago, DatosPago
+from .models import Agregados, Habitaciones, MediosPago, DatosPago, Reservaciones
+from .models import Transacciones, RelTransaccionReservacion, RelTransaccionAgregado
 
 # Register your models here.
 
 @admin.register(Habitaciones)
 class HabitacionesAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre', 'estado', 'costo', 'informacion')
+
 @admin.register(MediosPago)
 class MediosPagoAdmin(admin.ModelAdmin):
     list_display = ('id', 'descripcion')
+
 @admin.register(DatosPago)
 class DatosPagoAdmin(admin.ModelAdmin):
     list_display = ('id', 'informacion', 'medios_id')
+
+@admin.register(Reservaciones)
+class ReservacionesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fecha_reservada', 'costo_reservado','correo','datos_pago_id','habitaciones_id')
+
+@admin.register(Agregados)
+class AgregadosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'categoria', 'agregado','costo')
+
+@admin.register(Transacciones)
+class TransaccionesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fecha_transaccion', 'total','detalles')
+
+@admin.register(RelTransaccionReservacion)
+class RelTraResAdmin(admin.ModelAdmin):
+    list_display = ('transaccion_id', 'reservacion_id')
+
+@admin.register(RelTransaccionAgregado)
+class RelTraAgrAdmin(admin.ModelAdmin):
+    list_display = ('transaccion_id', 'agregado_id')
--- apps/home/models.py
+++ apps/home/models.py
@@ -3,36 +3,54 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+from statistics import mode
 from tkinter import CASCADE
+from tokenize import blank_re
 from django.db import models
 from django.contrib.auth.models import User
+
+# Formatos
+
+def dinero():
+    return models.DecimalField(
+        max_digits=10, 
+        decimal_places=2,
+        default=0,
+    )
+
+formato_dinero = dinero
+
+def llave(referencia):
+    return models.ForeignKey(
+        referencia, blank=False,
+        on_delete=models.RESTRICT,
+        null=True,
+    )
+
+formato_llave = llave
 
 # Tabla Habitaciones
 
 class Habitaciones(models.Model):
 
     class estadoHabitacion(models.TextChoices):
-        DISPONIBLE = 'DI', 'disponible'
-        OCUPADA = 'OC', 'ocupada'
-        MANTENIMIENTO = 'MA', 'mantenimiento'
-        LIMPIEZA = 'LI', 'limpieza'
-        DESHABILITADA = 'DE', 'deshabilitada'
+        DISPONIBLE = 'DI', 'Disponible'
+        OCUPADA = 'OC', 'Ocupada'
+        MANTENIMIENTO = 'MA', 'Mantenimiento'
+        LIMPIEZA = 'LI', 'Limpieza'
+        DESHABILITADA = 'DE', 'Deshabilitada'
 
-    nombre =  models.CharField(
-        max_length=50,
-        blank=False,
-    )
+    nombre =  models.CharField(max_length=50)
     estado = models.CharField(
         max_length=2,
         choices=estadoHabitacion.choices,
         default=estadoHabitacion.DESHABILITADA,
     )
-    costo = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2,
-        default=0,
-    )
+    costo = formato_dinero()
     informacion = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.nombre
 
 # Tabla Medios de Pago
 
@@ -52,7 +70,55 @@ class DatosPago(models.Model):
         max_length=250,
         default='Sin datos de pago',
     )
-    medios = models.ForeignKey(
-        MediosPago, blank=False,
-        on_delete=models.RESTRICT,
+    medios = formato_llave(MediosPago)
+
+    def __str__(self):
+        return self.informacion
+
+# Tabla Reservaciones
+
+class Reservaciones(models.Model):
+    fecha_reservada = models.DateField()
+    costo_reservado = formato_dinero()
+    correo = models.EmailField()
+    habitaciones = formato_llave(Habitaciones)
+    datos_pago = formato_llave(DatosPago)
+
+# Tabla Agregados
+
+class Agregados(models.Model):
+
+    class categoriaAgregado(models.TextChoices):
+        DESCONOCIDA = 'DE', 'Desconocida'
+        PRODUCTO = 'PR', 'Producto'
+        SERVICIO = 'SE', 'Servicio'
+
+    categoria = models.CharField(
+        max_length=2,
+        choices=categoriaAgregado.choices,
+        default=categoriaAgregado.DESCONOCIDA,
     )
+    agregado = models.CharField(max_length=50)
+    costo = formato_dinero()
+
+    def __str__(self):
+        return self.agregado
+
+# Tabla Transacciones
+
+class Transacciones(models.Model):
+    fecha_transaccion = models.DateField()
+    total = formato_dinero()
+    detalles = models.CharField(max_length=200)
+
+# Tabla Relacion Transaccion - Reservacion
+
+class RelTransaccionReservacion(models.Model):
+    transaccion = formato_llave(Transacciones)
+    reservacion = formato_llave(Reservaciones)
+
+# Tabla Relacion Transaccion - Agregados
+
+class RelTransaccionAgregado(models.Model):
+    transaccion = formato_llave(Transacciones)
+    agregado = formato_llave(Agregados)
