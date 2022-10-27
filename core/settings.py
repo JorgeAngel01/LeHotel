5ee3efe901ea935e8fe79350da171556c6fe7972
LeiWithP <pedro.barrita1029@gmail.com>
2022-10-27 00:09:38 -0500

    Modificaciones al html principal. Envio de correos agregado aun sin ser funcional

--- core/settings.py
+++ core/settings.py
@@ -169,3 +169,10 @@ if GITHUB_AUTH:
         }
     }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'leibarrita@gmail.com'
+EMAIL_HOST_PASSWORD = 'lousrfekryvnqptz'
+DEFAULT_FROM_EMAIL = 'email default'
