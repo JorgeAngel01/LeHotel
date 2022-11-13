0a6ea8f4eb3a6a4969d05a4f9541512e9ba4b7f4
Jorge Angel Diaz <jorge.angelus0107@gmail.com>
2022-11-13 15:22:23 -0600

    Reservation data saved in DB

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,15 +2,15 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-from venv import create
 from django.core import serializers
 from django import template
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from .models import Habitaciones, Reservaciones, Agregados
+from .models import Habitaciones, Reservaciones, Agregados, Transacciones, RelTransaccionAgregado , RelTransaccionReservacion
 from .forms import Reservacion
+from datetime import datetime
 
 
 # @login_required(login_url="/login/")
@@ -56,38 +56,48 @@ def reservacion(request, room_id):
     form = Reservacion(request.POST or None, room = room_id)
     #form = Reservacion(room = room_id)
 
-    if form.is_valid():
-        #form = Reservacion()
-        print(request.POST)
-  
-    """
-    if request.method == 'GET':
-        return HttpResponse(html_template.render(context, request))  
-    else:
-        print(request.POST)
-        print(request.POST['email'])
-        print(request.POST['nombres'])
-        print(request.POST['paterno'])
-        print(request.POST['materno'])
-        print(request.POST['fecha_ini'])
-        print(request.POST['fecha_fin'])
-        for a in agregados:
-            if a.agregado in request.POST: print(request.POST[a.agregado])
-
-
-        fecha_ini = datetime.strptime(request.POST['fecha_ini'],"%m/%d/%Y").strftime("%Y-%m-%d")
-        fecha_fin = datetime.strptime(request.POST['fecha_fin'],"%m/%d/%Y").strftime("%Y-%m-%d")
-
-        reserva = Reservaciones.objects.create(estado='PR', fecha_reserva = fecha_ini, fecha_entrega = fecha_fin, costo_reservado = request.POST['cost'], correo = request.POST['email'], habitaciones = room ) 
-    """
     context = {
         'data' : data,
         'room' : room,
         'reservacion': form,
         'agrega': agregados
     }  
-    
-    return HttpResponse(html_template.render(context, request))
+
+    if request.method == 'GET':
+        return HttpResponse(html_template.render(context, request))  
+    if request.method == 'POST':
+        if form.is_valid():
+            #form = Reservacion()
+            print(request.POST)
+            print(request.POST['email'])
+            print(request.POST['nombres'])
+            print(request.POST['paterno'])
+            print(request.POST['materno'])
+            print(request.POST['fecha_ini'])
+            print(request.POST['fecha_fin'])
+
+            # Reservacion object saved
+            fecha_ini = datetime.strptime(request.POST['fecha_ini'],"%d/%m/%Y").strftime("%Y-%m-%d")
+            fecha_fin = datetime.strptime(request.POST['fecha_fin'],"%d/%m/%Y").strftime("%Y-%m-%d")
+
+            reserva = Reservaciones.objects.create(estado='PR', fecha_reserva = fecha_ini, fecha_entrega = fecha_fin, costo_reservado = request.POST['cost'], correo = request.POST['email'], habitaciones = room ) 
+            # Transaccion object saved
+            nombre = "Cliente: " + request.POST['paterno'] + " " + request.POST['materno'] + " " + request.POST['nombres']
+
+            trans = Transacciones.objects.create(fecha_transaccion = datetime.now(), total = request.POST['cost'], detalles = nombre )
+
+            # Trnas-Reser relation saved
+            RelTransaccionReservacion.objects.create(transaccion= trans, reservacion = reserva)
+
+            costo = float(request.POST['cost'])
+            # Agregado-Trans relation saved
+            for a in agregados:
+                if a.agregado in request.POST:
+                    costo += float(a.costo)
+                    RelTransaccionAgregado.objects.create(transaccion = trans, agregado = a)
+            Transacciones.objects.filter(pk = trans.pk).update(total = costo)          
+
+        return HttpResponse(html_template.render(context, request))
 
 @login_required(login_url="/login/")
 def pages(request):
